0891f28560b699b512895e4060923df33d1cf1a2
Stutee Routray <stuteer@gmail.com>
2021-03-14 17:06:35 -0700

    package name modified

--- src/main/java/com/demo/springboot/SpringHibernateApplication.java
+++ src/main/java/com/demo/springboot/SpringHibernateApplication.java
@@ -1,4 +1,4 @@
-package com.demo.srpingboot;
+package com.demo.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
